3a27fe6556538587ed98087f65ee31206e96a862
preston.a.jackson <preston.a.jackson@861a406c-534a-0410-8894-cb66d6ee9925>
2009-08-21 14:00:34 +0000

    Cleaning up gtest.xcode. Removing old tests, using gtest-all.cc, adding a static libgtest.a and a static libgtest_main.a, fixing the sample code to work with changes.

--- trunk/xcode/Scripts/runtests.sh
+++ trunk/xcode/Scripts/runtests.sh
@@ -1,49 +1,16 @@
 #!/bin/bash
 
-# Executes the samples and tests for the Google Test Framework
+# Executes the samples and tests for the Google Test Framework.
 
-# Help the dynamic linker find the path to the framework 
+# Help the dynamic linker find the path to the libraries.
 export DYLD_FRAMEWORK_PATH=$BUILT_PRODUCTS_DIR
+export DYLD_LIBRARY_PATH=$BUILT_PRODUCTS_DIR
 
-# Create an array of test executables
-test_executables=("$BUILT_PRODUCTS_DIR/sample1_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample2_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample3_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample4_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample5_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample6_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample7_unittest"
-                  "$BUILT_PRODUCTS_DIR/sample8_unittest"
-
-                  "$BUILT_PRODUCTS_DIR/gtest-death-test_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_environment_test"
-                  "$BUILT_PRODUCTS_DIR/gtest-filepath_test"
-                  "$BUILT_PRODUCTS_DIR/gtest-linked_ptr_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_main_unittest"
-                  "$BUILT_PRODUCTS_DIR/gtest-message_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_no_test_unittest"
-                  "$BUILT_PRODUCTS_DIR/gtest-options_test"
-                  "$BUILT_PRODUCTS_DIR/gtest-param-test_test"
-                  "$BUILT_PRODUCTS_DIR/gtest-port_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_pred_impl_unittest"
-                  "$BUILT_PRODUCTS_DIR/gtest_prod_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_repeat_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_sole_header_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_stress_test"
-                  "$BUILT_PRODUCTS_DIR/gtest_test_part_test"
-                  "$BUILT_PRODUCTS_DIR/gtest-typed-test_test"
+# Create some executables.
+test_executables=("$BUILT_PRODUCTS_DIR/gtest_unittest-framework"
                   "$BUILT_PRODUCTS_DIR/gtest_unittest"
-
-                  "$BUILT_PRODUCTS_DIR/gtest_break_on_failure_unittest.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_color_test.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_env_var_test.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_filter_unittest.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_list_tests_unittest.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_output_test.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_xml_outfiles_test.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_xml_output_unittest.py"
-                  "$BUILT_PRODUCTS_DIR/gtest_uninitialized_test.py"
-)
+                  "$BUILT_PRODUCTS_DIR/sample1_unittest-framework"
+                  "$BUILT_PRODUCTS_DIR/sample1_unittest-static")
 
 # Now execute each one in turn keeping track of how many succeeded and failed. 
 succeeded=0
@@ -60,7 +27,7 @@ for test in ${test_executables[*]}; do
   fi
 done
 
-# Report the successes and failures to the console
+# Report the successes and failures to the console.
 echo "Tests complete with $succeeded successes and $failed failures."
 if [ $failed -ne 0 ]; then
   echo "The following tests failed:"
