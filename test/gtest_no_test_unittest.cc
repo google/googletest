682c89f7557eb53c7359b6cbf3670c05165f2419
zhanyong.wan <zhanyong.wan@861a406c-534a-0410-8894-cb66d6ee9925>
2010-06-16 22:47:13 +0000

    Makes gtest report failures in ad hoc test assertions executed before RUN_ALL_TESTS().

--- test/gtest_no_test_unittest.cc
+++ test/gtest_no_test_unittest.cc
@@ -40,15 +40,18 @@ int main(int argc, char **argv) {
 
   // An ad-hoc assertion outside of all tests.
   //
-  // This serves two purposes:
+  // This serves three purposes:
   //
   // 1. It verifies that an ad-hoc assertion can be executed even if
   //    no test is defined.
-  // 2. We had a bug where the XML output won't be generated if an
+  // 2. It verifies that a failed ad-hoc assertion causes the test
+  //    program to fail.
+  // 3. We had a bug where the XML output won't be generated if an
   //    assertion is executed before RUN_ALL_TESTS() is called, even
   //    though --gtest_output=xml is specified.  This makes sure the
   //    bug is fixed and doesn't regress.
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 2);
 
-  return RUN_ALL_TESTS();
+  // The above EXPECT_EQ() should cause RUN_ALL_TESTS() to return non-zero.
+  return RUN_ALL_TESTS() ? 0 : 1;
 }
