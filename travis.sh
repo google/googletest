9ce9d5d020b2d6cbcc624d6d04a9febc18bab921
James Macfarlane <jmacfarlane06@gmail.com>
2016-08-06 08:07:50 -0500

    update travis.sh to respect variables BUILD_TYPE and VERBOSE_MAKE

--- travis.sh
+++ travis.sh
@@ -9,7 +9,9 @@ cmake -Dgtest_build_samples=ON \
       -Dgmock_build_samples=ON \
       -Dgtest_build_tests=ON \
       -Dgmock_build_tests=ON \
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
+      -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE_MAKE \
       ../../$GTEST_TARGET
 make
 CTEST_OUTPUT_ON_FAILURE=1 make test
