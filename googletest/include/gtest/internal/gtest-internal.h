afed11e85023d2d7e8f16e8eccf66f0b52a76583
BrukerJWD <jonathan.wendeborn@bruker.com>
2016-10-19 11:14:26 +0200

    GTEST_TEST_NO_THROW_ outputs exception message

--- googletest/include/gtest/internal/gtest-internal.h
+++ googletest/include/gtest/internal/gtest-internal.h
@@ -1148,18 +1148,24 @@ class NativeArray {
       fail(gtest_msg.value)
 
 #define GTEST_TEST_NO_THROW_(statement, fail) \
+  std::string GTEST_CONCAT_TOKEN_(gtest_exmsg_testnothrow_,__LINE__); \
   GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
   if (::testing::internal::AlwaysTrue()) { \
     try { \
       GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
     } \
+    catch (const std::exception& e) { \
+      GTEST_CONCAT_TOKEN_(gtest_exmsg_testnothrow_,__LINE__) = e.what(); \
+      goto GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__); \
+    } \
     catch (...) { \
+      GTEST_CONCAT_TOKEN_(gtest_exmsg_testnothrow_,__LINE__) = "no std::exception"; \
       goto GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__); \
     } \
   } else \
     GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__): \
-      fail("Expected: " #statement " doesn't throw an exception.\n" \
-           "  Actual: it throws.")
+      fail(("Expected: " #statement " doesn't throw an exception.\n" \
+            "  Actual: it throws: " + GTEST_CONCAT_TOKEN_(gtest_exmsg_testnothrow_,__LINE__)).c_str())
 
 #define GTEST_TEST_ANY_THROW_(statement, fail) \
   GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
