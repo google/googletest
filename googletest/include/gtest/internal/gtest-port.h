ef9cadd5f7205e3f758016ffb94ca905aedfd43b
Saleem Abdulrasool <compnerd@compnerd.org>
2017-04-27 16:09:12 -0700

    gtest-port: do not use _LIBCPP_VERSION
    _LIBCPP_VERSION indicates that the underlying C++ runtime is libc++.
    However, it does not indicate what ABI underlies it.  libc++ can be
    built for Windows with the Microsoft (MSVC) ABI.  In this mode, cxxabi.h
    is not available.

--- googletest/include/gtest/internal/gtest-port.h
+++ googletest/include/gtest/internal/gtest-port.h
@@ -951,8 +951,7 @@ using ::std::tuple_size;
 # define GTEST_NO_INLINE_
 #endif
 
-// _LIBCPP_VERSION is defined by the libc++ library from the LLVM project.
-#if defined(__GLIBCXX__) || defined(_LIBCPP_VERSION)
+#if defined(__GLIBCXX__)
 # define GTEST_HAS_CXXABI_H_ 1
 #else
 # define GTEST_HAS_CXXABI_H_ 0
