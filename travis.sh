8364718500dac72c141aa55dcb2483177356717d
Herbert Thielen <thielen@hs-worms.de>
2017-08-29 16:35:41 +0200

    remove non-existing gmock_build_samples switch

--- travis.sh
+++ travis.sh
@@ -6,7 +6,6 @@ mkdir build || true
 mkdir build/$GTEST_TARGET || true
 cd build/$GTEST_TARGET
 cmake -Dgtest_build_samples=ON \
-      -Dgmock_build_samples=ON \
       -Dgtest_build_tests=ON \
       -Dgmock_build_tests=ON \
       -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
