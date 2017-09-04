5fece522d24344c0294f6d35ceff9aab6bcd94dd
Herbert Thielen <thielen@hs-worms.de>
2017-09-04 18:56:52 +0200

    limit processors to use in Travis build to 4

--- travis.sh
+++ travis.sh
@@ -5,10 +5,19 @@ set -evx
 # otherwise take 2 processors as reasonable default; see
 # https://docs.travis-ci.com/user/speeding-up-the-build/#Makefile-optimization
 if [ -x /usr/bin/getconf ]; then
-    MAKEFLAGS=j$(/usr/bin/getconf _NPROCESSORS_ONLN)
+    NPROCESSORS=$(/usr/bin/getconf _NPROCESSORS_ONLN)
 else
-    MAKEFLAGS="j2"
+    NPROCESSORS=2
 fi
+# as of 2017-09-04 Travis CI reports 32 processors, but GCC build
+# crashes if parallelized too much (maybe memory consumption problem),
+# so limit to 4 processors for the time being.
+if [ $NPROCESSORS -gt 4 ] ; then
+	echo "$0:Note: Limiting processors to use by make from $NPROCESSORS to 4."
+	NPROCESSORS=4
+fi
+# Tell make to use the processors. No preceding '-' required.
+MAKEFLAGS="j${NPROCESSORS}"
 export MAKEFLAGS
 
 env | sort
