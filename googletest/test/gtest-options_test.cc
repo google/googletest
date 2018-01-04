86b2831e693865d8597ee611294518cacd539a54
Unknown <genat@iname.com>
2018-01-04 15:40:26 -0500

    Code merge and cleanup, WIP

--- googletest/test/gtest-options_test.cc
+++ googletest/test/gtest-options_test.cc
@@ -27,7 +27,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Authors: keith.ray@gmail.com (Keith Ray)
 //
 // Google Test UnitTestOptions tests
 //
