39118dc707e75e98bd2d63fd00145843c04e3ab2
Knut Omang <knut.omang@oracle.com>
2018-01-29 14:09:34 +0100

    Remove / from parameterized test names if base test name is empty
    This is useful to let a subsystem generate test names based on parameterized tests
    without exposing them as such to the user.
    
    Signed-off-by: Knut Omang <knut.omang@oracle.com>

--- googletest/include/gtest/internal/gtest-param-util.h
+++ googletest/include/gtest/internal/gtest-param-util.h
@@ -564,7 +564,10 @@ class ParameterizedTestSuiteInfo : public ParameterizedTestSuiteInfoBase {
 
           test_param_names.insert(param_name);
 
-          test_name_stream << test_info->test_base_name << "/" << param_name;
+	  if (test_info->test_base_name.size() > 0)
+	    test_name_stream << test_info->test_base_name << "/" << param_name;
+	  else
+	    test_name_stream << test_info->test_base_name << param_name;
           MakeAndRegisterTestInfo(
               test_suite_name.c_str(), test_name_stream.GetString().c_str(),
               nullptr,  // No type parameter.
