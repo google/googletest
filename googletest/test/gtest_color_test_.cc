736dd6745c87935ee793c54d4efa29f6d9600cab
jstrs <37332179+jstrs@users.noreply.github.com>
2018-03-13 19:20:37 +0000

    Update gtest_color_test_.cc

--- googletest/test/gtest_color_test_.cc
+++ googletest/test/gtest_color_test_.cc
@@ -50,8 +50,8 @@ TEST(GTestColorTest, Dummy) {
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
-  RUN_ALL_TESTS();
-
+  if (RUN_ALL_TESTS() != 0)
+    return -1;
   if (ShouldUseColor(true)) {
     // Google Test decides to use colors in the output (assuming it
     // goes to a TTY).
