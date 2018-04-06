d9f3611a227cf51eb8028b07e58ba330d3a04092
Gennadiy Civil <misterg@google.com>
2018-04-06 11:17:45 -0400

    more MSVC warnings

--- googletest/include/gtest/gtest.h
+++ googletest/include/gtest/gtest.h
@@ -82,6 +82,13 @@
 
 namespace testing {
 
+// Silence C4100 (unreferenced formal parameter) for MSVC
+#ifdef _MSC_VER
+# pragma warning(push)
+# pragma warning(disable:4100)
+#endif
+
+
 // Declares the flags.
 
 // This flag temporary enables the disabled tests.
@@ -2297,6 +2304,10 @@ bool StaticAssertTypeEq() {
 // Returns a path to temporary directory.
 // Tries to determine an appropriate directory for the platform.
 GTEST_API_ std::string TempDir();
+
+#ifdef _MSC_VER
+# pragma warning(pop)
+#endif
 
 }  // namespace testing
 
