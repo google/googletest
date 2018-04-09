a958171f8a7d9336013d10afd1626865da5c81b3
Gennadiy Civil <misterg@google.com>
2018-04-09 10:25:59 -0400

    preproc syntax ( I can never remember it)

--- googlemock/src/gmock-spec-builders.cc
+++ googlemock/src/gmock-spec-builders.cc
@@ -51,9 +51,11 @@
 
 // Silence C4800 (C4800: 'int *const ': forcing value
 // to bool 'true' or 'false') for MSVC 14
-#ifdef _MSC_VER && (_MSC_VER == 1900)
-# pragma warning(push)
-# pragma warning(disable:4800)
+#ifdef _MSC_VER
+#if _MSC_VER == 1900
+#  pragma warning(push)
+#  pragma warning(disable:4800)
+#endif
 #endif
 
 namespace testing {
@@ -874,6 +876,8 @@ InSequence::~InSequence() {
 
 }  // namespace testing
 
-#ifdef _MSC_VER && (_MSC_VER == 1900)
-# pragma warning(pop)
+#ifdef _MSC_VER
+#if _MSC_VER == 1900
+#  pragma warning(pop)
+#endif
 #endif
