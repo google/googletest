1944bc0f510fa631b8d35075b4ff95c3efeacf39
Gennadiy Civil <misterg@google.com>
2018-04-16 11:41:36 -0400

    typo

--- googlemock/test/gmock-generated-matchers_test.cc
+++ googlemock/test/gmock-generated-matchers_test.cc
@@ -35,7 +35,7 @@
 // possible loss of data
 #ifdef _MSC_VER
 # pragma warning(push)
-# pragma warning(disable:C4244)
+# pragma warning(disable:4244)
 #endif
 
 #include "gmock/gmock-generated-matchers.h"
