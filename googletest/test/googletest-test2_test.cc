f3511bf1c703c31c226cab29bd04106cdeb5f2ac
Gennadiy Civil <misterg@google.com>
2018-08-02 14:56:33 -0400

    cleaning up and adding test changes to CMake

--- googletest/test/googletest-test2_test.cc
+++ googletest/test/googletest-test2_test.cc
@@ -40,11 +40,11 @@ using ::testing::internal::ParamGenerator;
 
 // Tests that generators defined in a different translation unit
 // are functional. The test using extern_gen_2 is defined
-// in gtest-param-test_test.cc.
+// in googletest-param-test-test.cc.
 ParamGenerator<int> extern_gen_2 = Values(33);
 
 // Tests that a parameterized test case can be defined in one translation unit
-// and instantiated in another. The test is defined in gtest-param-test_test.cc
+// and instantiated in another. The test is defined in googletest-param-test-test.cc
 // and ExternalInstantiationTest fixture class is defined in
 // gtest-param-test_test.h.
 INSTANTIATE_TEST_CASE_P(MultiplesOf33,
@@ -53,7 +53,7 @@ INSTANTIATE_TEST_CASE_P(MultiplesOf33,
 
 // Tests that a parameterized test case can be instantiated
 // in multiple translation units. Another instantiation is defined
-// in gtest-param-test_test.cc and InstantiationInMultipleTranslaionUnitsTest
+// in googletest-param-test-test.cc and InstantiationInMultipleTranslaionUnitsTest
 // fixture is defined in gtest-param-test_test.h
 INSTANTIATE_TEST_CASE_P(Sequence2,
                         InstantiationInMultipleTranslaionUnitsTest,
