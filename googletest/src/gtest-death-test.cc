ecc6944fb127be957185731e1dfbfd6eafb02d0e
Gennadiy Civil <misterg@google.com>
2018-08-09 11:12:12 -0400

    Fixing identation, causes build errors when warnings are treated as errors

--- googletest/src/gtest-death-test.cc
+++ googletest/src/gtest-death-test.cc
@@ -238,8 +238,8 @@ static std::string DeathTestThreadWarning(size_t thread_count) {
   else
     msg << "detected " << thread_count << " threads.";
     msg << " See https://github.com/google/googletest/blob/master/googletest/docs/advanced.md#death-tests-and-threads"
-      << " for more explanation and suggested solutions, especially if"
-      << " this is the last message you see before your test times out.";
+        << " for more explanation and suggested solutions, especially if"
+        << " this is the last message you see before your test times out.";
   return msg.GetString();
 }
 # endif  // !GTEST_OS_WINDOWS && !GTEST_OS_FUCHSIA
