f11ec7dd52c8ed83be52371bcbdc14b5bc3ea800
misterg <misterg@google.com>
2018-08-20 14:48:03 -0400

    googletest export
      - 209457486 Import of OSS PR, https://github.com/google/googletest/pu... by misterg <misterg@google.com>
    
    PiperOrigin-RevId: 209457486

--- googletest/samples/sample4_unittest.cc
+++ googletest/samples/sample4_unittest.cc
@@ -37,12 +37,17 @@ namespace {
 TEST(Counter, Increment) {
   Counter c;
 
+  // Test that counter 0 returns 0
+  EXPECT_EQ(0, c.Decrement());
+
   // EXPECT_EQ() evaluates its arguments exactly once, so they
   // can have side effects.
 
   EXPECT_EQ(0, c.Increment());
   EXPECT_EQ(1, c.Increment());
   EXPECT_EQ(2, c.Increment());
+
+  EXPECT_EQ(3, c.Decrement());
 }
 
 }  // namespace
