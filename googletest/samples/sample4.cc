cfc0d5fb0c59b6e15a991f21cf970e557a698e22
Gennadiy Civil <misterg@google.com>
2018-08-20 14:17:38 -0400

    Testing, adding a few line to sample4

--- googletest/samples/sample4.cc
+++ googletest/samples/sample4.cc
@@ -38,6 +38,16 @@ int Counter::Increment() {
   return counter_++;
 }
 
+// Returns the current counter value, and decrements it.
+// counter can not be less than 0, return 0 in this case
+  int Counter::Decrement() {
+    if (counter_==0){
+      return counter_;
+    }
+    else
+      return counter_--;
+  }
+
 // Prints the current counter value to STDOUT.
 void Counter::Print() const {
   printf("%d", counter_);
