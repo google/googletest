69f8d58db833a93b85afec70f2db4b396c90de77
Abseil Team <absl-team@google.com>
2018-08-27 14:53:25 -0400

    Googletest export
    Fix Theta(N^2) memory usage of EXPECT_EQ(string) when the strings don't match.
    
    The underlying CalculateOptimalEdits() implementation used a simple
    dynamic-programming approach that always used N^2 memory and time. This meant
    that tests for equality of large strings were ticking time bombs: They'd work
    fine as long as the test passed, but as soon as the strings differed the test
    would OOM, which is very hard to debug.
    I switched it out for a Dijkstra search, which is still worst-case O(N^2), but
    in the usual case of mostly-matching strings, it is much closer to linear.
    
    PiperOrigin-RevId: 210405025

--- googletest/src/gtest-all.cc
+++ googletest/src/gtest-all.cc
@@ -38,10 +38,11 @@
 #include "gtest/gtest.h"
 
 // The following lines pull in the real gtest *.cc files.
-#include "src/gtest.cc"
 #include "src/gtest-death-test.cc"
+#include "src/gtest-edit-distance.cc"
 #include "src/gtest-filepath.cc"
 #include "src/gtest-port.cc"
 #include "src/gtest-printers.cc"
 #include "src/gtest-test-part.cc"
 #include "src/gtest-typed-test.cc"
+#include "src/gtest.cc"
