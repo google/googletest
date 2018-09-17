f058218569a77a77cbb45a4ce1296d139f72de08
Matthieu Longo <mateo.longo@gmail.com>
2018-09-17 18:17:10 +0200

    [msys] fix unittest ColoredOutputTest.UsesColorsWhenTermSupportsColors

--- googletest/include/gtest/internal/gtest-port-arch.h
+++ googletest/include/gtest/internal/gtest-port-arch.h
@@ -38,14 +38,15 @@
 // Determines the platform on which Google Test is compiled.
 #ifdef __CYGWIN__
 # define GTEST_OS_CYGWIN 1
+# elif defined(__MINGW__) || defined(__MINGW32__) || defined(__MINGW64__)
+#  define GTEST_OS_WINDOWS_MINGW 1
+#  define GTEST_OS_WINDOWS 1
 #elif defined __SYMBIAN32__
 # define GTEST_OS_SYMBIAN 1
 #elif defined _WIN32
 # define GTEST_OS_WINDOWS 1
 # ifdef _WIN32_WCE
 #  define GTEST_OS_WINDOWS_MOBILE 1
-# elif defined(__MINGW__) || defined(__MINGW32__)
-#  define GTEST_OS_WINDOWS_MINGW 1
 # elif defined(WINAPI_FAMILY)
 #  include <winapifamily.h>
 #  if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
