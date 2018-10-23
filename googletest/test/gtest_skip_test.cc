5a8c6314000139b4b38942512757b18fca7af86f
durandal <durandal@google.com>
2018-10-23 15:31:17 -0400

    Googletest export
    Honor GTEST_SKIP() in SetUp().
    
    PiperOrigin-RevId: 218387359

--- googletest/test/gtest_skip_test.cc
+++ googletest/test/gtest_skip_test.cc
@@ -32,7 +32,24 @@
 
 #include "gtest/gtest.h"
 
+using ::testing::Test;
+
 TEST(SkipTest, DoesSkip) {
   GTEST_SKIP();
   EXPECT_EQ(0, 1);
+}
+
+class Fixture : public Test {
+ protected:
+  void SetUp() override {
+    GTEST_SKIP() << "skipping all tests for this fixture";
+  }
+};
+
+TEST_F(Fixture, SkipsOneTest) {
+  EXPECT_EQ(5, 7);
+}
+
+TEST_F(Fixture, SkipsAnotherTest) {
+  EXPECT_EQ(99, 100);
 }
