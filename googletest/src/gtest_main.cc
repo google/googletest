53527583a88fc70dedbd4b47d123fc2d874a23b7
Chris <chrisjohnsonmail@gmail.com>
2019-01-03 12:12:19 -0600

    fix:  Remove Arduino entry points
    Improved flexibility by removing the Arduino entry points in favor of manual calls to setup/loop that the user can call from their entry point.  This is the more common use case for Arudino.
    
    Also added the gtest/gmock_main files to the PlatformIO ignore list since we are not supporting that feature.

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -30,24 +30,8 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-#ifdef ARDUINO
-void setup() {
-  // Since Arduino doesn't have a command line, fake out the argc/argv arguments
-  int argc = 1;
-  const auto arg0 = "PlatformIO";
-  char* argv0 = const_cast<char*>(arg0);
-  char** argv = &argv0;
-
-  testing::InitGoogleTest(&argc, argv);
-}
-
-void loop() { RUN_ALL_TESTS(); }
-
-#else
-
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-#endif
