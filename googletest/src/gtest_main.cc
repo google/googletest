4a0fef4ecf9e177d9f6aadcfed96f93a53eae50e
Chris <chrisjohnsonmail@gmail.com>
2019-01-05 11:21:56 -0600

    clang-tidy: modernize-deprecated-headers

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -27,7 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdio.h>
+#include <cstdio>
 #include "gtest/gtest.h"
 
 #ifdef ARDUINO
