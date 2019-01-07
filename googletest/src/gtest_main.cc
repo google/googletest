9536b82e870d6761ca9a15f990d669ab24cb2d47
Chris <chrisjohnsonmail@gmail.com>
2019-01-07 12:37:34 -0600

    fix:  Add Arduino setup()/loop() functions back
    Added setup()/loop() functions back to *_main.cc files to support compiling in CI.  Future features could enable this for the end user.

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -30,8 +30,22 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
+#ifdef ARDUINO
+void setup() {
+  int argc = 0;
+  char** argv = nullptr;
+  testing::InitGoogleTest(&argc, argv);
+}
+
+void loop() { RUN_ALL_TESTS(); }
+
+#else
+
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+#endif
+
