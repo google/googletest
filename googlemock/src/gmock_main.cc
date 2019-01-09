cc240c56b4b460d77f11822e82699581a45409d5
user706 <39215612+user706@users.noreply.github.com>
2019-01-09 23:18:11 +0100

    no dllimport on main (remove macro GTEST_API_ from before main)

--- googlemock/src/gmock_main.cc
+++ googlemock/src/gmock_main.cc
@@ -57,9 +57,9 @@ void loop() { RUN_ALL_TESTS(); }
 #if GTEST_OS_WINDOWS_MOBILE
 # include <tchar.h>  // NOLINT
 
-GTEST_API_ int _tmain(int argc, TCHAR** argv) {
+int _tmain(int argc, TCHAR** argv) {
 #else
-GTEST_API_ int main(int argc, char** argv) {
+int main(int argc, char** argv) {
 #endif  // GTEST_OS_WINDOWS_MOBILE
   std::cout << "Running main() from gmock_main.cc\n";
   // Since Google Mock depends on Google Test, InitGoogleMock() is
