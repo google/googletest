6c7f37e1bbc45b746127c8a940e07cb80fae6ba8
Abseil Team <absl-team@google.com>
2019-02-01 19:24:17 -0500

    Export Test - Do Not Merge
    Update example code in gtest.h to prefer override over virtual now
    that it is widely available in C++11.
    
    PiperOrigin-RevId: 232057792

--- googletest/include/gtest/gtest.h
+++ googletest/include/gtest/gtest.h
@@ -1854,13 +1854,13 @@ GTEST_API_ GTEST_ATTRIBUTE_PRINTF_(2, 3) void ColoredPrintf(GTestColor color,
 //   FooTest() {
 //     // Can use GetParam() here.
 //   }
-//   virtual ~FooTest() {
+//   ~FooTest() override {
 //     // Can use GetParam() here.
 //   }
-//   virtual void SetUp() {
+//   void SetUp() override {
 //     // Can use GetParam() here.
 //   }
-//   virtual void TearDown {
+//   void TearDown override {
 //     // Can use GetParam() here.
 //   }
 // };
@@ -2380,7 +2380,7 @@ bool StaticAssertTypeEq() {
 //
 //   class FooTest : public testing::Test {
 //    protected:
-//     virtual void SetUp() { b_.AddElement(3); }
+//     void SetUp() override { b_.AddElement(3); }
 //
 //     Foo a_;
 //     Foo b_;
