cc8788e8b093f82b2e5ebbde6270d35efde2c98b
misterg <misterg@google.com>
2019-06-18 14:21:57 -0400

    Googletest export
    Move Googletest OSS bazel builds to latest bazel.
    The latest bazel ( 0.27.0 as of now ) does not run on trusty.
    
    PiperOrigin-RevId: 253829774

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -41,10 +41,7 @@ if [ "${TRAVIS_SUDO}" = "true" ]; then
     echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | \
         sudo tee /etc/apt/sources.list.d/bazel.list
     curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-    sudo apt-get update && sudo apt-get install -y gcc-4.9 g++-4.9 clang-3.9
-    sudo wget https://github.com/bazelbuild/bazel/releases/download/0.26.1/bazel-0.26.1-installer-linux-x86_64.sh
-    sudo chmod +x bazel-0.26.1-installer-linux-x86_64.sh
-    sudo ./bazel-0.26.1-installer-linux-x86_64.sh
+    sudo apt-get update && sudo apt-get install -y bazel gcc-4.9 g++-4.9 clang-3.9
 elif [ "${CXX}" = "clang++" ]; then
     # Use ccache, assuming $HOME/bin is in the path, which is true in the Travis build environment.
     ln -sf /usr/bin/ccache $HOME/bin/${CXX};
