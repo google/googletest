a7b131e1f2ac8eff392f1bae86f6d28a75ffee41
Peter Steneteg <peter@steneteg.se>
2019-06-26 11:04:28 +0200

    Removes export of main function. Closes #891

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -39,7 +39,7 @@ void loop() { RUN_ALL_TESTS(); }
 
 #else
 
-GTEST_API_ int main(int argc, char **argv) {
+int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
