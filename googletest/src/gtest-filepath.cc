3f05f651ae3621db58468153e32016bc1397800b
Abseil Team <absl-team@google.com>
2019-09-05 18:41:05 -0400

    Googletest export
    Extend gtest-port and stubs for ESP_PLATFORM
    
    ESP_PLATFORM is the macro used to indicate compilation for the ESP32
    using the esp-idf. This isn't a fully posix compatible system so
    various features of google test need to be stubbed out in order for
    it to work. It's oddly similar to the GTEST_OS_WINDOWS_PHONE setup.
    
    PiperOrigin-RevId: 267471968

--- googletest/src/gtest-filepath.cc
+++ googletest/src/gtest-filepath.cc
@@ -93,8 +93,8 @@ static bool IsPathSeparator(char c) {
 // Returns the current working directory, or "" if unsuccessful.
 FilePath FilePath::GetCurrentDir() {
 #if GTEST_OS_WINDOWS_MOBILE || GTEST_OS_WINDOWS_PHONE || \
-    GTEST_OS_WINDOWS_RT || ARDUINO
-  // Windows CE and Arduino don't have a current directory, so we just return
+    GTEST_OS_WINDOWS_RT || ARDUINO || defined(ESP_PLATFORM)
+  // These platforms do not have a current directory, so we just return
   // something reasonable.
   return FilePath(kCurrentDirectoryString);
 #elif GTEST_OS_WINDOWS
