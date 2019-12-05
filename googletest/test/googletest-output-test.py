321fb9d543c27f1edd3f44defc242b0a41f1b69a
Abseil Team <absl-team@google.com>
2019-12-05 07:26:54 -0500

    Googletest export
    Internal change
    
    PiperOrigin-RevId: 283948137

--- googletest/test/googletest-output-test.py
+++ googletest/test/googletest-output-test.py
@@ -29,7 +29,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Tests the text output of Google C++ Testing and Mocking Framework.
+r"""Tests the text output of Google C++ Testing and Mocking Framework.
 
 To update the golden file:
 googletest_output_test.py --build_dir=BUILD/DIR --gengolden
