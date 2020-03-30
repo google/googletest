aea3ece93f6d2b2203faa9546d01ac45b7a20170
Abseil Team <absl-team@google.com>
2020-03-30 14:26:22 -0400

    Googletest export
    Do not use std::result_of as it was removed in C++20.
    
    PiperOrigin-RevId: 303783600

--- googlemock/include/gmock/gmock-actions.h
+++ googlemock/include/gmock/gmock-actions.h
@@ -970,7 +970,8 @@ struct InvokeMethodWithoutArgsAction {
   Class* const obj_ptr;
   const MethodPtr method_ptr;
 
-  using ReturnType = typename std::result_of<MethodPtr(Class*)>::type;
+  using ReturnType =
+      decltype((std::declval<Class*>()->*std::declval<MethodPtr>())());
 
   template <typename... Args>
   ReturnType operator()(const Args&...) const {
