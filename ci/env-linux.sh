9a5ea077322af4d143eee417af757bfafb33620b
dmauro <dmauro@google.com>
2020-05-28 15:37:07 -0400

    Googletest export
    Update Travis CI to use Ubuntu Bionic in some cases
    
    PiperOrigin-RevId: 313639562

--- ci/env-linux.sh
+++ ci/env-linux.sh
@@ -36,6 +36,6 @@
 # TODO() - we can check if this is being sourced using $BASH_VERSION and $BASH_SOURCE[0] != ${0}.
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
-    if [ "$CXX" = "clang++" ]; then export CXX="clang++-3.9" CC="clang-3.9"; fi
+    if [ "$CXX" = "g++" ]; then export CXX="g++" CC="gcc"; fi
+    if [ "$CXX" = "clang++" ]; then export CXX="clang++" CC="clang"; fi
 fi
