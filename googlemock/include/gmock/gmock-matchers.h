f04d7017cb7faadd0c1e12bc023bbccce7bf0253
Vladimir Goncharov <amatanhead@yandex-team.ru>
2020-07-03 20:50:06 +0300

    Add missing documentation piece

--- googlemock/include/gmock/gmock-matchers.h
+++ googlemock/include/gmock/gmock-matchers.h
@@ -4814,7 +4814,8 @@ class ExceptionMatcherImpl {
 //
 //   EXPECT_THAT(
 //       []() { throw std::runtime_error("message"); },
-//       Throws
+//       Throws<std::runtime_error>(
+//           Property(&std::runtime_error::what, HasSubstr("message"))));
 
 template <typename Err>
 PolymorphicMatcher<internal::ExceptionMatcherImpl<Err>>
