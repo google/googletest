4679637f1c9d5a0728bdc347a531737fad0b1ca3
tbarbier <thomas.barbier@amadeus.com>
2020-09-24 16:51:06 +0200

    Fix warning maybe-uninitialized

--- googletest/src/gtest-death-test.cc
+++ googletest/src/gtest-death-test.cc
@@ -1281,7 +1281,7 @@ static void StackLowerThanAddress(const void* ptr,
 GTEST_ATTRIBUTE_NO_SANITIZE_ADDRESS_
 GTEST_ATTRIBUTE_NO_SANITIZE_HWADDRESS_
 static void StackLowerThanAddress(const void* ptr, bool* result) {
-  int dummy;
+  int dummy = 0;
   *result = (&dummy < ptr);
 }
 
@@ -1289,7 +1289,7 @@ static void StackLowerThanAddress(const void* ptr, bool* result) {
 GTEST_ATTRIBUTE_NO_SANITIZE_ADDRESS_
 GTEST_ATTRIBUTE_NO_SANITIZE_HWADDRESS_
 static bool StackGrowsDown() {
-  int dummy;
+  int dummy = 0;
   bool result;
   StackLowerThanAddress(&dummy, &result);
   return result;
