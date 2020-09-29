a83a670a125d1c2d52ba89766377e8f75ee5665b
dmauro <dmauro@google.com>
2020-09-29 15:27:38 -0400

    Googletest export
    Fix undefined pointer comparison
    
    PiperOrigin-RevId: 334436975

--- googletest/src/gtest-death-test.cc
+++ googletest/src/gtest-death-test.cc
@@ -32,6 +32,7 @@
 
 #include "gtest/gtest-death-test.h"
 
+#include <functional>
 #include <utility>
 
 #include "gtest/internal/gtest-port.h"
@@ -1286,7 +1287,7 @@ GTEST_ATTRIBUTE_NO_SANITIZE_ADDRESS_
 GTEST_ATTRIBUTE_NO_SANITIZE_HWADDRESS_
 static void StackLowerThanAddress(const void* ptr, bool* result) {
   int dummy = 0;
-  *result = (&dummy < ptr);
+  *result = std::less<const void*>()(&dummy, ptr);
 }
 
 // Make sure AddressSanitizer does not tamper with the stack here.
