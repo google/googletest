a4ccf5707c7b5a7ecba62dbae37a83cc0e8e6034
Abseil Team <absl-team@google.com>
2021-04-13 00:30:43 -0700

    Export Test - Do Not Merge
    Add support to run gtest on Xtensa platform.
    
    This add support to run GTest base test suits on Xtensa
    (https://ip.cadence.com/ipportfolio/tensilica-ip) base simulator.
    Xtensa only provides libc and some basic operations and does not run an
    operating system by default.
    
    PiperOrigin-RevId: 368162205

--- googletest/src/gtest-filepath.cc
+++ googletest/src/gtest-filepath.cc
@@ -92,8 +92,9 @@ static bool IsPathSeparator(char c) {
 
 // Returns the current working directory, or "" if unsuccessful.
 FilePath FilePath::GetCurrentDir() {
-#if GTEST_OS_WINDOWS_MOBILE || GTEST_OS_WINDOWS_PHONE || \
-    GTEST_OS_WINDOWS_RT || GTEST_OS_ESP8266 || GTEST_OS_ESP32
+#if GTEST_OS_WINDOWS_MOBILE || GTEST_OS_WINDOWS_PHONE ||         \
+    GTEST_OS_WINDOWS_RT || GTEST_OS_ESP8266 || GTEST_OS_ESP32 || \
+    GTEST_OS_XTENSA
   // These platforms do not have a current directory, so we just return
   // something reasonable.
   return FilePath(kCurrentDirectoryString);
@@ -323,7 +324,7 @@ bool FilePath::CreateFolder() const {
   delete [] unicode;
 #elif GTEST_OS_WINDOWS
   int result = _mkdir(pathname_.c_str());
-#elif GTEST_OS_ESP8266
+#elif GTEST_OS_ESP8266 || GTEST_OS_XTENSA
   // do nothing
   int result = 0;
 #else
