155de14cd861a29886e85a1f98407d93e16b0595
Jeremy Nimmer <jeremy.nimmer@tri.global>
2021-07-01 11:43:07 -0700

    Use GTEST_DONT_DEFINE_TEST_F to guard TEST_F
    The documentation is clear that the FOO we'll be guarding always matches
    the spelling of the DONT macro.  A single guard macro should not toggle
    more than one implementation macro.
    
    This fixes a regression in 7413280c52c1f759395572a384165023d24eeb57.
    
    Relatedly, improve the documentation of the DONT macros to bring the
    list of valid FOO values up to date.

--- googletest/include/gtest/gtest.h
+++ googletest/include/gtest/gtest.h
@@ -2379,12 +2379,13 @@ constexpr bool StaticAssertTypeEq() noexcept {
 //     EXPECT_EQ(b_.size(), 1);
 //   }
 //
-// GOOGLETEST_CM0011 DO NOT DELETE
-#if !GTEST_DONT_DEFINE_TEST
-#define TEST_F(test_fixture, test_name)\
+#define GTEST_TEST_F(test_fixture, test_name)\
   GTEST_TEST_(test_fixture, test_name, test_fixture, \
               ::testing::internal::GetTypeId<test_fixture>())
-#endif  // !GTEST_DONT_DEFINE_TEST
+// GOOGLETEST_CM0011 DO NOT DELETE
+#if !GTEST_DONT_DEFINE_TEST_F
+#define TEST_F(test_fixture, test_name) GTEST_TEST_F(test_fixture, test_name)
+#endif
 
 // Returns a path to temporary directory.
 // Tries to determine an appropriate directory for the platform.
