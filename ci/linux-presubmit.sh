cd14b68fcfa23f2cf3ec27d83a1249e130cd2ffd
Abseil Team <absl-team@google.com>
2021-10-11 18:08:23 -0400

    Export Test - Do Not Merge
    Use newer Docker images in CI
    
    The new floor image contains GCC 5.5.0, like the old one.
    
    PiperOrigin-RevId: 402402465

--- ci/linux-presubmit.sh
+++ ci/linux-presubmit.sh
@@ -31,8 +31,8 @@
 
 set -euox pipefail
 
-readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20210525"
-readonly LINUX_GCC_FLOOR_CONTAINER="gcr.io/google.com/absl-177019/linux_gcc-floor:20201015"
+readonly LINUX_LATEST_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20210617"
+readonly LINUX_GCC_FLOOR_CONTAINER="gcr.io/google.com/absl-177019/linux_gcc-floor:20210617"
 
 if [[ -z ${GTEST_ROOT:-} ]]; then
   GTEST_ROOT="$(realpath $(dirname ${0})/..)"
