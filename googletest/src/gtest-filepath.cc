b3a016720d6bb23275d14a8863abd9c03699bf6e
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-22 21:33:01 +0400

    Update gtest-filepath.cc

--- googletest/src/gtest-filepath.cc
+++ googletest/src/gtest-filepath.cc
@@ -1,10 +1,8 @@
 // Copyright 2008, Google Inc.
 // All rights reserved.
-//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-//
 //     * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above
