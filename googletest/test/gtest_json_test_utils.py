ee5e3043ce54939c31e6b7e14ca6cb69cc5bf18f
Szymon Sobik <sobik.szymon@gmail.com>
2022-03-08 01:14:21 +0100

    Add support for testing of xml and json output of source file and line location

--- googletest/test/gtest_json_test_utils.py
+++ googletest/test/gtest_json_test_utils.py
@@ -50,6 +50,8 @@ def normalize(obj):
     elif key == 'failure':
       value = re.sub(r'^.*[/\\](.*:)\d+\n', '\\1*\n', value)
       return re.sub(r'Stack trace:\n(.|\n)*', 'Stack trace:\n*', value)
+    elif key == 'file':
+      return re.sub(r'^.*[/\\](.*)', '\\1', value)
     else:
       return normalize(value)
   if isinstance(obj, dict):
