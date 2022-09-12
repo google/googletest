cd80ab87ae5fc4d466bd7a844fe67e697adb2b98
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-09-13 00:54:06 +0800

    Add extern "C" to main entry point
    since the bare metal or RTOS environment need demangled name
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: Ic971a80e248a40e28ce7897c00f07c082e798cbb

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -45,7 +45,7 @@ void loop() { RUN_ALL_TESTS(); }
 
 #else
 
-GTEST_API_ int main(int argc, char **argv) {
+extern "C" GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
