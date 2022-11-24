6f1c4b3d7b139c7217698bf4115be9f40785f661
Yonggang Luo <luoyonggang@gmail.com>
2022-11-24 23:54:19 +0800

    Fixes the test gmock_output_test.py with MSVC
    For MSVC, gmock_output_test.py output struct std::pair<int,bool>, for GCC, it's output
    std::pair<int, bool>, it's not the same, my intention is getting these to be same by removing
    struct  for MSVC's outptu, and strip redundant space for GCC.
    
    As a by-product,
    ```
    #ifdef _MSC_VER
    #define ERROR_DESC "class std::runtime_error"
    #else
    #define ERROR_DESC "std::runtime_error"
    #endif
    ```
    can be simplified to
    
    ```
    #define ERROR_DESC "std::runtime_error"
    ```
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- googletest/test/gtest_unittest.cc
+++ googletest/test/gtest_unittest.cc
@@ -3314,11 +3314,7 @@ TEST_F(SingleEvaluationTest, OtherCases) {
 
 #if GTEST_HAS_RTTI
 
-#ifdef _MSC_VER
-#define ERROR_DESC "class std::runtime_error"
-#else
 #define ERROR_DESC "std::runtime_error"
-#endif
 
 #else  // GTEST_HAS_RTTI
 
