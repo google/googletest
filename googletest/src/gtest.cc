934542165899c786cb5d8a710529c37184730183
Tom Hughes <tomhughes@google.com>
2023-01-05 11:14:51 -0800

    Fix GTEST_OS_ESP8266 check
    Everywhere else in the code, we check the value of GTEST_OS_ESP8266, not just
    whether it is defined.
    
    PiperOrigin-RevId: 499946909
    Change-Id: I86f7e8947abb4e928fc24d1416d8237987b27845

--- googletest/src/gtest.cc
+++ googletest/src/gtest.cc
@@ -5030,7 +5030,7 @@ class ScopedPrematureExitFile {
   }
 
   ~ScopedPrematureExitFile() {
-#if !defined GTEST_OS_ESP8266
+#if !GTEST_OS_ESP8266
     if (!premature_exit_filepath_.empty()) {
       int retval = remove(premature_exit_filepath_.c_str());
       if (retval) {
