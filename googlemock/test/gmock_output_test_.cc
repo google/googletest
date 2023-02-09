097f64e98693500d44d022a781745e562aa492ad
Yonggang Luo <luoyonggang@gmail.com>
2023-02-09 15:29:01 +0800

    Revert "Fix gmock_output_test when using MSVC"
    This reverts commit 0a3b403fe037ff80daa1826ae99eed41e94dea05.

--- googlemock/test/gmock_output_test_.cc
+++ googlemock/test/gmock_output_test_.cc
@@ -33,7 +33,6 @@
 #include <stdio.h>
 
 #include <string>
-#include <tuple>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -255,16 +254,12 @@ TEST_F(GMockOutputTest, CatchesLeakedMocks) {
 }
 
 MATCHER_P2(IsPair, first, second, "") {
-  return Value(std::get<0>(arg), first) && Value(std::get<1>(arg), second);
+  return Value(arg.first, first) && Value(arg.second, second);
 }
 
 TEST_F(GMockOutputTest, PrintsMatcher) {
   const testing::Matcher<int> m1 = Ge(48);
-  // Explicitly using std::tuple instead of std::pair due to differences between
-  // MSVC and other compilers. std::pair is printed as
-  // "struct std::pair<int,bool>" when using MSVC vs "std::pair<int,bool>" with
-  // other compilers.
-  EXPECT_THAT((std::tuple<int, bool>(42, true)), IsPair(m1, true));
+  EXPECT_THAT((std::pair<int, bool>(42, true)), IsPair(m1, true));
 }
 
 void TestCatchesLeakedMocksInAdHocTests() {
