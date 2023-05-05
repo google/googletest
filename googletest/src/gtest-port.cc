bc860af08783b8113005ca7697da5f5d49a8056f
Abseil Team <absl-team@google.com>
2023-05-05 11:04:05 -0700

    This trips up when compiling with -Wvla otherwise.
    PiperOrigin-RevId: 529762901
    Change-Id: I6ce4d630191bf265f847aef2d5dcc12a712faa60

--- googletest/src/gtest-port.cc
+++ googletest/src/gtest-port.cc
@@ -199,8 +199,8 @@ size_t GetThreadCount() {
   mib[5] = static_cast<int>(size / static_cast<size_t>(mib[4]));
 
   // populate array of structs
-  struct kinfo_proc info[mib[5]];
-  if (sysctl(mib, miblen, &info, &size, NULL, 0)) {
+  std::vector<struct kinfo_proc> info(mib[5]);
+  if (sysctl(mib, miblen, info.data(), &size, NULL, 0)) {
     return 0;
   }
 
