e6c256a80a5fd4345109340f581d941edcd16376
gen7 <giuseppe2.gentile@mail.polimi.it>
2023-05-10 13:19:15 +0200

    Raz

--- googletest/samples/sample1.cc
+++ googletest/samples/sample1.cc
@@ -4,7 +4,7 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-//
+// Ratto
 //     * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above
