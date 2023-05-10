6c7a8238779f9435c24034b1b9109a570d3fa924
matteo-riga <mtrigam@gmail.com>
2023-05-10 13:31:46 +0200

    hpce test 415

--- googletest/samples/sample2.cc
+++ googletest/samples/sample2.cc
@@ -33,9 +33,13 @@
 
 #include <string.h>
 
+#include <iostream>
+
 // Clones a 0-terminated C string, allocating memory using new.
 const char* MyString::CloneCString(const char* a_c_string) {
   if (a_c_string == nullptr) return nullptr;
+
+  std::cout << "HPCE test #415" << std::endl;
 
   const size_t len = strlen(a_c_string);
   char* const clone = new char[len + 1];
