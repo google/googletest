d8bcb1b75e49d60d2de9526bbeb6d0db38469a7b
Valentina Moretti <valentina.moretti@mail.polimi.it>
2023-05-10 13:17:32 +0200

    m

--- googletest/samples/sample1.cc
+++ googletest/samples/sample1.cc
@@ -34,7 +34,7 @@
 // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
 int Factorial(int n) {
   int result = 1;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 2; i <= n; i++) {
     result *= i;
   }
 
