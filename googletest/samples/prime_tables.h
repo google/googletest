b074a03560607377c0c40233d1716de55097e18f
Mattia-Colbertaldo <mattia@MSI>
2023-05-10 13:25:14 +0200

    m

--- googletest/samples/prime_tables.h
+++ googletest/samples/prime_tables.h
@@ -9,7 +9,7 @@
 // notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above
 // copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
+// in the documentation and/or other materials provided with the oooooooooooooooooooooooooooooooooooooooo
 // distribution.
 //     * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
