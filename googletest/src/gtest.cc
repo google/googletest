e4b6d065c0d6a32dcb1f3e4e770d22bdd917a54d
Dan Avram <avrdan@gmail.com>
2023-10-29 21:11:59 +0200

    [#4294] --gtest_break_on_failure conflicts with EXPECT_NONFATAL_FAILURE.

--- googletest/src/gtest.cc
+++ googletest/src/gtest.cc
@@ -5352,7 +5352,8 @@ void UnitTest::AddTestPartResult(TestPartResult::Type result_type,
       result);
 
   if (result_type != TestPartResult::kSuccess &&
-      result_type != TestPartResult::kSkip) {
+      result_type != TestPartResult::kSkip &&
+      result_type != TestPartResult::kNonFatalFailure) {
     // gtest_break_on_failure takes precedence over
     // gtest_throw_on_failure.  This allows a user to set the latter
     // in the code (perhaps in order to use Google Test assertions
