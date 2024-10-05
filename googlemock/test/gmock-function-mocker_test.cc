71815bbf7de6e10c11821d654a2fae2cf42de0f7
Abseil Team <absl-team@google.com>
2024-10-04 20:53:30 -0700

    Automated Code Change
    PiperOrigin-RevId: 682549952
    Change-Id: Ica81f0020c3001c01543a5851dae7d0b42730c66

--- googlemock/test/gmock-function-mocker_test.cc
+++ googlemock/test/gmock-function-mocker_test.cc
@@ -325,8 +325,8 @@ TYPED_TEST(FunctionMockerTest, MocksBinaryFunction) {
 
 // Tests mocking a decimal function.
 TYPED_TEST(FunctionMockerTest, MocksDecimalFunction) {
-  EXPECT_CALL(this->mock_foo_,
-              Decimal(true, 'a', 0, 0, 1L, A<float>(), Lt(100), 5U, NULL, "hi"))
+  EXPECT_CALL(this->mock_foo_, Decimal(true, 'a', 0, 0, 1L, A<float>(), Lt(100),
+                                       5U, nullptr, "hi"))
       .WillOnce(Return(5));
 
   EXPECT_EQ(5, this->foo_->Decimal(true, 'a', 0, 0, 1, 0, 0, 5, nullptr, "hi"));
