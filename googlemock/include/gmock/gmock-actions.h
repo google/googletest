50a9511f5013e2e02e791ed04813a57d7cdf5d45
Aaron Jacobs <jacobsa@google.com>
2025-04-09 15:33:04 -0700

    `gmock-actions`: run clang-format.
    PiperOrigin-RevId: 745757973
    Change-Id: I59ca0098a73eaf8a7e68016ba003fc2447e1c702

--- googlemock/include/gmock/gmock-actions.h
+++ googlemock/include/gmock/gmock-actions.h
@@ -1707,9 +1707,8 @@ template <size_t k>
 struct ReturnArgAction {
   template <typename... Args,
             typename = typename std::enable_if<(k < sizeof...(Args))>::type>
-  auto operator()(Args&&... args) const
-      -> decltype(std::get<k>(
-          std::forward_as_tuple(std::forward<Args>(args)...))) {
+  auto operator()(Args&&... args) const -> decltype(std::get<k>(
+      std::forward_as_tuple(std::forward<Args>(args)...))) {
     return std::get<k>(std::forward_as_tuple(std::forward<Args>(args)...));
   }
 };
