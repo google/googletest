d844fa23a0bc0630e3588a48154c0c51228d39c0
Hoyong Lee <ghdyd6372@naver.com>
2026-01-20 17:35:23 +0900

    Fix null pointer crash in ScopedPrematureExitFile when file creation fails
      When TEST_PREMATURE_EXIT_FILE is set and posix::FOpen fails,
      fwrite and
      fclose are called on nullptr without null check.
    
      Add null check before fwrite and fclose to ignore file
      creation failures
      as intended by the existing comment.

--- googletest/src/gtest.cc
+++ googletest/src/gtest.cc
@@ -5146,8 +5146,10 @@ class ScopedPrematureExitFile {
       // errors are ignored as there's nothing better we can do and we
       // don't want to fail the test because of this.
       FILE* pfile = posix::FOpen(premature_exit_filepath_.c_str(), "w");
-      fwrite("0", 1, 1, pfile);
-      fclose(pfile);
+      if (pfile != nullptr) {
+        fwrite("0", 1, 1, pfile);
+        fclose(pfile);
+      }
     }
   }
 
